12b18b1e2763d1f03e6dda298181872ff5ddf984
Ptopic <petartopic2002@gmail.com>
2024-07-27 23:37:11 +0200

    Seed users

--- ProjectManagement/Data/AppDbContext.cs
+++ ProjectManagement/Data/AppDbContext.cs
@@ -29,5 +29,143 @@ public class AppDbContext : IdentityDbContext<User>
         user.NormalizedName = "USER";
 
         builder.Entity<IdentityRole>().HasData(admin, manager, user);
+
+        PasswordHasher<User> passwordHasher = new PasswordHasher<User>();
+        
+        var admin1 = new User
+        {
+            Id = "97e0ae24-f7e2-4ad5-a1ee-570856fcf874",
+            UserName = "admin@gmail.com",
+            NormalizedUserName = "ADMIN@GMAIL.COM",
+            Email = "admin@gmail.com",
+            NormalizedEmail = "ADMIN@GMAIL.COM",
+            EmailConfirmed = true,
+            SecurityStamp = Guid.NewGuid().ToString("D"),
+            FirstName = "Admin",
+            LastName = "Admin",
+        };
+        admin1.PasswordHash = passwordHasher.HashPassword(admin1, "Petar123$");
+
+        var manager1 = new User
+        {
+            Id = "95fd4abd-0626-49fd-956c-19438402a603",
+            UserName = "manager@gmail.com",
+            NormalizedUserName = "MANAGER@GMAIL.COM",
+            Email = "manager@gmail.com",
+            NormalizedEmail = "MANAGER@GMAIL.COM",
+            EmailConfirmed = true,
+            SecurityStamp = Guid.NewGuid().ToString("D"),
+            FirstName = "Manager",
+            LastName = "Manager",
+        };
+        manager1.PasswordHash = passwordHasher.HashPassword(manager1, "Petar123$");
+
+        var manager2 = new User
+        {
+            Id = "1f7ec708-e841-4ab2-846f-05c24fa18301",
+            UserName = "petar.topic@blankhq.co",
+            NormalizedUserName = "PETAR.TOPIC@BLANKHQ.CO",
+            Email = "petar.topic@blankhq.co",
+            NormalizedEmail = "PETAR.TOPIC@BLANKHQ.CO",
+            EmailConfirmed = true,
+            SecurityStamp = Guid.NewGuid().ToString("D"),
+            FirstName = "Manager2",
+            LastName = "Manager2",
+        };
+        manager2.PasswordHash = passwordHasher.HashPassword(manager2, "Petar123$");
+
+        var user1 = new User
+        {
+            Id = "e58ea87d-6a8f-49e0-939c-20d89f995574",
+            UserName = "petartopic2002@gmail.com",
+            NormalizedUserName = "PETARTOPIC2002@GMAIL.COM",
+            Email = "petartopic2002@gmail.com",
+            NormalizedEmail = "PETARTOPIC2002@GMAIL.COM",
+            EmailConfirmed = true,
+            SecurityStamp = Guid.NewGuid().ToString("D"),
+            FirstName = "User",
+            LastName = "User",
+        };
+        user1.PasswordHash = passwordHasher.HashPassword(user1, "Petar123$");
+
+        var user2 = new User
+        {
+            Id = "f8d480c5-ebe1-45e3-a804-a9badf390003",
+            UserName = "user2@gmail.com",
+            NormalizedUserName = "USER2@GMAIL.COM",
+            Email = "user2@gmail.com",
+            NormalizedEmail = "USER2@GMAIL.COM",
+            EmailConfirmed = true,
+            SecurityStamp = Guid.NewGuid().ToString("D"),
+            FirstName = "User2",
+            LastName = "User2",
+        };
+        user2.PasswordHash = passwordHasher.HashPassword(user2, "Petar123$");
+
+        var user3 = new User
+        {
+            Id = "c33ee900-c261-4195-b8d6-2ac802657d95",
+            UserName = "user3@gmail.com",
+            NormalizedUserName = "USER3@GMAIL.COM",
+            Email = "user3@gmail.com",
+            NormalizedEmail = "USER3@GMAIL.COM",
+            EmailConfirmed = true,
+            SecurityStamp = Guid.NewGuid().ToString("D"),
+            FirstName = "User3",
+            LastName = "User3",
+        };
+        user3.PasswordHash = passwordHasher.HashPassword(user3, "Petar123$");
+
+        var user4 = new User
+        {
+            Id = "318f8ee8-cb12-4d95-9123-1d911a167f3f",
+            UserName = "user4@gmail.com",
+            NormalizedUserName = "USER4@GMAIL.COM",
+            Email = "user4@gmail.com",
+            NormalizedEmail = "USER4@GMAIL.COM",
+            EmailConfirmed = true,
+            SecurityStamp = Guid.NewGuid().ToString("D"),
+            FirstName = "User4",
+            LastName = "User4",
+        };
+        
+        user4.PasswordHash = passwordHasher.HashPassword(user4, "Petar123$");
+        
+        builder.Entity<User>().HasData(admin1, manager1, manager2, user1, user2, user3, user4);
+        
+        builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string> { 
+            RoleId = admin.Id, 
+            UserId = admin1.Id, 
+        });
+        
+        builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string> { 
+            RoleId = manager.Id, 
+            UserId = manager1.Id, 
+        });
+        
+        builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string> { 
+            RoleId = manager.Id, 
+            UserId = manager2.Id, 
+        });
+        
+        builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string> { 
+            RoleId = user.Id, 
+            UserId = user1.Id, 
+        });
+        
+        builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string> { 
+            RoleId = user.Id, 
+            UserId = user2.Id, 
+        });
+        
+        builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string> { 
+            RoleId = user.Id, 
+            UserId = user3.Id, 
+        });
+        
+        builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string> { 
+            RoleId = user.Id, 
+            UserId = user4.Id, 
+        });
     }
 }
