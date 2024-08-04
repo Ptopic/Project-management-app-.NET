88110d04e25aaae94b0f127c10f0b2c0d8775b24
Petar Topic <petartopic2002@gmail.com>
2024-08-04 23:03:14 +0200

    Delete user

---
+++ ProjectManagement/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectManagement.Entities;
+
+namespace ProjectManagement.Pages.Users;
+
+[Authorize(Roles = ("Admin"))]
+public class Delete(UserManager<User> _userManager) : PageModel
+{
+    public string Username { get; set; }
+    
+    public async Task<IActionResult> OnGet(string Id)
+    {
+        var user = await _userManager.FindByIdAsync(Id);
+        if (user == null)
+        {
+            return base.BadRequest($"Unable to load user with ID '{Id}'.");
+        }
+
+        var userName = await _userManager.GetUserNameAsync(user);
+        Username = userName;
+
+        return Page();
+    }
+    
+    public async Task<IActionResult> OnPostAsync(string Id)
+    {
+        var user = await _userManager.FindByIdAsync(Id);
+        if (user == null)
+        {
+            return base.BadRequest($"Unable to load user with ID '{Id}'.");
+        }
+
+        var result = await _userManager.DeleteAsync(user);
+        if (!result.Succeeded)
+        {
+            throw new InvalidOperationException($"Unexpected error occurred deleting user.");
+        }
+
+        return Redirect("~/Users/Index");
+    }
+}
