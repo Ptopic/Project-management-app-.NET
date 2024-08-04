0b962b68140b645d3553e73c50b5329a992743a2
Petar Topic <petartopic2002@gmail.com>
2024-08-05 01:13:32 +0200

    Add pagination to teams page

--- ProjectManagement/Pages/Teams/Index.cshtml.cs
+++ ProjectManagement/Pages/Teams/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProjectManagement.Entities.Enums;
+using ProjectManagement.Models;
 using ProjectManagement.Models.Views.Team;
 using ProjectManagement.Models.Views.Users;
 using ProjectManagement.Services;
@@ -12,20 +13,22 @@ public class Index : PageModel
 {
     private readonly ITeamService _teamService;
 
-    public IEnumerable<TeamView> Teams { get; private set; } = new List<TeamView>();
+    public PaginatedList<TeamView> Teams { get; set; }
 
     public Index(ITeamService teamService)
     {
         _teamService = teamService;
     }
     
-    public async Task<PageResult> OnGetAsync(string searchString, string currentFilter)
+    public async Task<PageResult> OnGetAsync(string searchString, string currentFilter, int? pageIndex)
     {
-        Teams = await _teamService.GetAllAsync();
+        var teams = await _teamService.GetAllAsync();
         
         ViewData["Keyword"] = searchString;
-        Teams = _teamService.Search(Teams, searchString);
+        teams = _teamService.Search(teams, searchString);
 
+        Teams = PaginatedList<TeamView>.Create(teams, pageIndex ?? 1, 5);
+        
         return Page();
     }
 }
