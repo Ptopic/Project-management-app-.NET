5021e9c7f3fc21a88acd9da9a0fa8530edcfb52b
Petar Topic <petartopic2002@gmail.com>
2024-08-20 23:54:58 +0200

    Fix design

--- ProjectManagement/Pages/Projects/Details.cshtml.cs
+++ ProjectManagement/Pages/Projects/Details.cshtml.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectManagement.Entities;
+using ProjectManagement.Services;
 
 namespace ProjectManagement.Pages.Projects;
 
-public class Details : PageModel
+public class Details(IProjectService _projectService) : PageModel
 {
-    public void OnGet()
+    public Project Project { get; set; }
+    
+    public async Task<IActionResult> OnGetAsync(string id)
     {
-        
+        var project = await _projectService.GetByIdAsync(id);
+        if (project == null)
+        {
+            return base.BadRequest($"Unable to load project with ID '{id}'.");
+        }
+
+        Project = project;
+
+        return Page();
     }
 }
