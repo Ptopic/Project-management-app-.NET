44972b2e1db047fa4471206298af3075fcda2e13
Petar Topic <petartopic2002@gmail.com>
2024-08-22 15:39:54 +0200

    Add check to task details

--- ProjectManagement/Pages/Tasks/Details.cshtml.cs
+++ ProjectManagement/Pages/Tasks/Details.cshtml.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProjectManagement.Entities;
+using ProjectManagement.Entities.Enums;
 using ProjectManagement.Models.Requests.Task;
 using ProjectManagement.Services;
 using TaskStatus = ProjectManagement.Entities.Enums.TaskStatus;
 
 namespace ProjectManagement.Pages.Tasks;
 
-public class Details(ITaskService _taskService, IProjectService _projectService, IUserService _userService, ITeamService _teamService) : PageModel
+public class Details(ITaskService _taskService, IProjectService _projectService, IUserService _userService, ITeamService _teamService, UserManager<User> _userManager) : PageModel
 {
     [BindProperty]
     public UpdateTaskRequest Input { get; set; }
@@ -44,6 +46,45 @@ public class Details(ITaskService _taskService, IProjectService _projectService,
         }
         
         Project = project;
+        
+        if (User.IsInRole(Roles.User.ToString()))
+        {
+            var projectTeamId = project.Team.Id;
+            
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            }
+            
+            var isUserMemberOfTeam = _teamService.IsUserMemberOfTeam(projectTeamId.ToString(), user);
+
+            if (!isUserMemberOfTeam)
+            {
+                return RedirectToPage("/Projects/Index");
+            }
+        }
+
+        if (User.IsInRole(Roles.Manager.ToString()))
+        {
+            var projectManager = project.Manager;
+            
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            }
+
+            if (projectManager != user)
+            {
+                var isManagerMemberOfTeam = _teamService.IsUserMemberOfTeam(project.Team.Id.ToString(), user);
+                
+                if (!isManagerMemberOfTeam)
+                {
+                    return RedirectToPage("/Projects/Index");
+                }
+            }
+        }
         
         var userOptions = new List<User>();
         
